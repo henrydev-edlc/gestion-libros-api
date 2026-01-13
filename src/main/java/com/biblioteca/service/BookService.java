dfc6e4dfe49d2501b420b751305b89c01f76cb51
henrydev-edlc <henrydev.edlc@gmail.com>
2026-01-13 14:02:18 -0600

    feat: implement book service for business logic

--- src/main/java/com/biblioteca/service/BookService.java
+++ src/main/java/com/biblioteca/service/BookService.java
@@ -1,4 +1,37 @@
 package com.biblioteca.service;
 
-public interface BookService {
+import com.biblioteca.model.Book;
+import com.biblioteca.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Page<Book> getAll(Pageable pageable) {
+        return bookRepository.findAll(pageable);
+    }
+
+    public Optional<Book> getById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void delete(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    public Page<Book> search(String query, Pageable pageable) {
+        return bookRepository.findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(query, query, pageable);
+    }
 }
