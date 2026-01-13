321fb49d07d0cefc9cbd0ff662e6abed260e0ab7
henrydev-edlc <henrydev.edlc@gmail.com>
2026-01-13 14:00:24 -0600

    feat: create book repository with search and pagination support

--- src/main/java/com/biblioteca/repository/BookRepository.java
+++ src/main/java/com/biblioteca/repository/BookRepository.java
@@ -1,4 +1,15 @@
 package com.biblioteca.repository;
 
-public class BookRepository {
+import com.biblioteca.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Page<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(
+            String title,
+            String author,
+            Pageable pageable
+    );
 }
